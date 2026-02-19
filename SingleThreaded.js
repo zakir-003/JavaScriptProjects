4fabe958fa9c33d08bf0b26e36c561554204b2a9
zakir003 <zakiransari5652@gmail.com>
2026-02-19 15:02:13 +0500

    All done in the file

--- SingleThreaded.js
+++ SingleThreaded.js
@@ -10,5 +10,6 @@ console.log(2);
 console.log(3);
 console.log(4);
 console.log(5);
+
 /* The execution of codes line by line one by one is called Singlethreaded. 
     JavaScript is Single threaded in nature  */
