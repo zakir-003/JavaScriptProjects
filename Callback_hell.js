8980da6f34019bdde4488e6fba8c4ac7c6f97001
zakirali003 <zakir.ali@invozone.dev>
2026-02-23 23:55:54 +0500

    created new file callback hell.js and clear the concepts of nested callbacks form callback hell

--- Callback_hell.js
+++ Callback_hell.js
@@ -2,4 +2,31 @@
 // Example:
 //Create async function using callback:Get User → Get Orders → Get Order Details
 
+function getUser(userId, callback) {
+  setTimeout(() => {
+    console.log('User fetched');
+    callback({ id: userId, name: 'Zakir' });
+  }, 1000);
+}
 
+function getOrders(userId, callback) {
+  setTimeout(() => {
+    console.log('Orders fetched');
+    callback([{ orderId: 101 }]);
+  }, 1000);
+}
+
+function getOrderDetails(orderId, callback) {
+  setTimeout(() => {
+    console.log('Order details fetched');
+    callback({ orderId: orderId, product: 'Laptop' });
+  }, 1000);
+}
+
+getUser(1, function (user) {
+  getOrders(user.id, function (orders) {
+    getOrderDetails(orders[0].orderId, function (details) {
+      console.log('Final:', details);
+    });
+  });
+});
