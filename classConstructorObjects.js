1ec30925c51d4bcbf3e105acd9d46388795564d0
zakirali003 <zakir.ali@invozone.dev>
2026-02-23 11:07:14 +0500

    final update

--- classConstructorObjects.js
+++ classConstructorObjects.js
@@ -1,8 +1,9 @@
 class Person {
-  constructor(fname, lname, contact) {
+  constructor(fname, lname, contact, address) {
     this.fname = fname;
     this.lname = lname;
     this.contact = contact;
+    this.adddress = address;
   }
   getName() {
     console.log(this.fname, this.lname);
@@ -11,9 +12,9 @@ class Person {
     console.log(this.contact);
   }
 }
-p1 = new Person('Zakir', 'ansari', 99999);
-p2 = new Person('Ali', 'Ahmed', 8888);
-p3 = new Person('sajid', 'Hussain', 776767);
+p1 = new Person('Zakir', 'ansari', 99999, 'Hargisa');
+p2 = new Person('Ali', 'Ahmed', 8888, 'Sakrdu');
+p3 = new Person('sajid', 'Hussain', 776767, 'Lahore');
 
 p1.getName, p1.getContact();
 p2.getName();
