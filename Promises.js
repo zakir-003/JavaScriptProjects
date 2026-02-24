28e384ba61589e412b81a9be0c0a9fb808e96987
zakirali003 <zakir.ali@invozone.dev>
2026-02-24 22:13:15 +0500

    Modified promise.js file and solved the question given by trainer Safdar sab i.e. resolve  the promise after 2 sec

--- Promises.js
+++ Promises.js
@@ -1,11 +1,23 @@
-let promise = new Promise((res, rej) => {
+// ++++++Another example of promise in conditional statemtnet +++++++++++
+let promise = new Promise((resolve, rej) => {
   console.log('This is Promise');
-  let num = 41;
-  if (num % 2 == 0) res('The number is even');
+  let num = 4;
+  if (num % 2 == 0) resolve('The number is even');
   else rej('the number is odd!');
 
-  res(123);
+  resolve(123);
   rej('Data not found!');
 });
-promise.then((res) => console.log(res));
-promise.catch((rej) => console.log(rej));
+promise.then((res) => console.log(res)); //   .then k zariye resolve chalta hai
+promise.catch((rej) => console.log(rej)); //  .catch sa reject part ko chalate hain
+
+// Create a promise that resovle after 2 seconds
+function AsyncFunc() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('success');
+    }, 2000);
+  });
+}
+let p1 = AsyncFunc();
+p1.then((res) => console.log(res)); // res k ander resolve wala value aye ga uper sa.
